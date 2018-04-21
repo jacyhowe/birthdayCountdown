bad3c39e2234ea5302f8dcc0f3a6edf2512862dc
jacyhowe <jacyhowe@hotmail.com>
2018-04-21 17:05:29 -0600

    added time calculations

--- src/components/clock.js
+++ src/components/clock.js
@@ -5,11 +5,38 @@ import React, { Component } from 'react';
     constructor(props) {
         super(props)
 
+        this.getTimeRemaining = this.getTimeRemaining.bind(this);
+
         this.state = {
-            timeRemaining: {}
+            timeRemaining: this.getTimeRemaining(this.props.birthdayFormState.startDate.toString())
+            
         }
     }
 
+    getTimeRemaining(birthday){
+
+        var bday = new Date(birthday)
+        let today = new Date();
+
+        var distance = bday.getTime() - today.getTime();
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        return {
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+
+    }
+
+    componentWillMount() {
+        
+    }
    
     
     render() {
@@ -17,17 +44,14 @@ import React, { Component } from 'react';
 
         return (
             <div>                
-                <div>DAYS{data.days}</div>
-                <div>HRS{data.hours}</div>
-                <div>MINS{data.minutes}</div>
-                <div>SECS{data.seconds}</div>
+                <div>DAYS {data.days}</div>
+                <div>HRS {data.hours}</div>
+                <div>MINS {data.minutes}</div>
+                <div>SECS {data.seconds}</div>
             </div>
         )
     }
 
-    componentDidMount() {
-        this.birthday = this.props.birthdayFormState.startDate.toString();
-    }
  }
 
  export default Clock;
