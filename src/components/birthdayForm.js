9e8894131b3f6007758da214ad1ce582e42697b7
jacyhowe <jacyhowe@hotmail.com>
2018-04-23 20:28:34 -0600

    added datepicker styles

--- src/components/birthdayForm.js
+++ src/components/birthdayForm.js
@@ -24,10 +24,11 @@ class BirthdayForm extends Component {
     });
 }
 
-    handleGenerate() {
+    handleGenerate(event) {
         this.setState({
         formCompleted: true
         })   
+        event.preventDefault();
     }
 
     handleChangeDate() {
@@ -38,25 +39,26 @@ class BirthdayForm extends Component {
     
     render() { 
         return (
-            <div>                
+            <form onSubmit={this.handleGenerate} className="birthday-container">                
                 {
                     this.state.formCompleted ? 
-                        <div>
+                        <div className="clock-container">
                             <Clock birthdayFormState={this.state}/>
                             <a className="change-date" onClick ={this.handleChangeDate}>Change Date</a>
                         </div>                    
                     :
-                        <div> 
-                            <DatePicker 
+                        <div className="datepicker-container"> 
+                            <DatePicker className="datepicker"
                                 selected={this.state.startDate}
                                 onChange={this.handleChange}
                             />
-                            <a onClick={this.handleGenerate}>Generate Countdown</a>
+                            <div className="submit-container">
+                                <input type="submit" value="Generate Countdown"/>                          </div>
                         </div>
                 }
                                 
                 
-            </div>
+            </form>
         )
     
    }
